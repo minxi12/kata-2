6179229ee4f0f80805093636ea56a162f3980e8e
Lou Minxi <Lou Minxi@DESKTOP-M69RIDK>
2021-10-06 12:17:58 +0100

    Kata2 Version 3

--- src/kata02_44/Histogram.java
+++ src/kata02_44/Histogram.java
@@ -1,10 +1,24 @@
 package kata02_44;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Histogram {
-    private int[] data;
+    private int data[];
     
-    public Histogram(){
-        
+    public Histogram(int[] data){
+        this.data = data;
     }
-   
+    
+    public int[] getData(){
+        return this.data;
+    }
+    
+    public Map getHistogram(){
+        Map<Integer, Integer>histogram = new HashMap<Integer, Integer>();
+        for (int key : this.data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
+        }
+        return histogram;
+    }
 }
